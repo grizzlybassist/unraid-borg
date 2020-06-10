f2d75eab62aff549c2b581b0ee17d53b028ba7e7
grizzlybassist <66695015+grizzlybassist@users.noreply.github.com>
2020-06-10 09:33:34 -0500

    Update borg-backup.sh

--- borg-backup.sh
+++ borg-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-LOGFILE="/boot/logs/Borg.txt"
+LOGFILE="/mnt/user/Cronjob/logs/borg-backup/borg-$(data "=%Y-%m-%d).txt"
 
 # Close if rclone/borg running
 if pgrep "borg" || pgrep "rclone" > /dev/null 
@@ -19,18 +19,18 @@ fi
 
 
 #This is the location your Borg program will store the backup data to
-export BORG_REPO='/YOURREPODESTINATION'
+export BORG_REPO='ssh://root@192.168.2.202/mnt/user/borg-backup'
 
 #This is the location you want Rclone to send the BORG_REPO to 
-export CLOUDDEST='GDrive:/YOURRCLONEGDRIVEFOLDERFORBORG'
+export CLOUDDEST='b2:/kile-nas1'
 
 #Speeds up the backup by caching more
 #export BORG_FILES_CACHE_TTL=26
 
 #Setting this, so you won't be asked for your repository passphrase:
-export BORG_PASSPHRASE='YOURREPOKEY'
+export BORG_PASSPHRASE=''
 
-export BORG_CACHE_DIR='/mnt/user/appdata/borg/cache/'
+export BORG_CACHE_DIR='/mnt/user0/appdata/borg/cache/'
 export BORG_BASE_DIR='/mnt/user/appdata/borg/'
 
 #Backup my unraid directories into an archive
@@ -48,16 +48,15 @@ borg create                         \
     --show-rc                       \
     --compression lz4               \
     --exclude-caches                \
-    --exclude /mnt/user/myfirstexcludeddirectory              \
-    --exclude /mnt/user/mysecondexcludedpdirectory             \
-    --exclude /mnt/user/mythirdexcludeddirectory              \
+    --exclude /mnt/user/Downloads/Movies             \
     \
     $BORG_REPO::'{hostname}-{now}'  \
     \
-    /mnt/user/myfirstbackupdirectory              \
-    /mnt/user/mysecondbackupdirectory             \
-    /mnt/user/mythirdtbackupdirectory              \
-    /mnt/user/myfourthbackupdirectory              \
+    /mnt/user/Cronjobs              \
+    /mnt/user/Photos                \
+    /mnt/user/kilefam               \
+    /mnt/user/ryan.kile
+    
     
     >> $LOGFILE 2>&1
 
@@ -72,9 +71,10 @@ borg prune                          \
     --list                          \
     --prefix '{hostname}-'          \
     --show-rc                       \
-    --keep-daily    7               \
-    --keep-weekly   4               \
-    --keep-monthly  6               \
+    --keep-daily    14              \
+    --keep-weekly   12              \
+    --keep-monthly  12              \
+    --keep-yearly   3               \
     >> $LOGFILE 2>&1
 
 prune_exit=$?
@@ -91,10 +91,10 @@ then
 
 #Reset timer
     SECONDS=0
-    echo "$(date "+%m-%d-%Y %T") : Rclone Borg sync has started" >> $LOGFILE
-    rclone sync $BORG_REPO $CLOUDDEST -P --stats 1s -v 2>&1 | tee -a $LOGFILE
-    rclonestart=$SECONDS   
-    echo "$(date "+%m-%d-%Y %T") : Rclone Borg sync completed in  $(($rclonestart/ 3600))h:$(($rclonestart% 3600/60))m:$(($rclonestart% 60))s" 2>&1 | tee -a $LOGFILE
+#    echo "$(date "+%m-%d-%Y %T") : Rclone Borg sync has started" >> $LOGFILE
+#    rclone sync $BORG_REPO $CLOUDDEST -P --stats 1s -v 2>&1 | tee -a $LOGFILE
+#    rclonestart=$SECONDS   
+#    echo "$(date "+%m-%d-%Y %T") : Rclone Borg sync completed in  $(($rclonestart/ 3600))h:$(($rclonestart% 3600/60))m:$(($rclonestart% 60))s" 2>&1 | tee -a $LOGFILE
 # All other errors
 else
     echo "$(date "+%m-%d-%Y %T") : Borg has errors code:" $global_exit 2>&1 | tee -a $LOGFILE
